51d35a6c28a04f928f7088c91795abacd2e89b46
Douglas Moura <douglas12345.dsm@gmail.com>
2025-07-10 15:47:18 -0300

    init: Commit Inicial do Projeto
    - Estrutura de pastas definida
    - Tecnologias instaladas:
        WebPack
        TypeScript
        TailwindCSS/PostCSS

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development', // ou 'production'
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', // Injeta CSS no DOM durante o desenvolvimento
+                    'css-loader', // Lê arquivos CSS
+                    'postcss-loader', // Processa o CSS com plugins (incluindo Tailwind)
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        })
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+        open: true
+    }
+}
