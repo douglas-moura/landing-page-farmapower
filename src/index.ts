49ac7771c6de11d48c5e8483a1e662d1a5340578
Douglas Moura <douglas12345.dsm@gmail.com>
2025-07-14 10:08:31 -0300

    style: Primeira versão de layuot
    - Menu Lateral
        Função de abrir e fechar menu
    - Barra Superior
    - Banner
        Imagem de planos de fundo
    - Título Padrão
    - Classe tailwind próprias inseridas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import './assets/css/style.css'
 
-console.log('Olá Mundo')
+const toggleMenu = (): void => {
+    console.log('menu alterado')
+    const menuContainer = document.getElementById('menu') as HTMLElement
+    menuContainer.classList.toggle('-translate-x-full')
+    menuContainer.classList.toggle('-left-8')
+}
+
+const abrirMenu = document.getElementById('icone-abrir-menu') as HTMLElement
+abrirMenu.addEventListener('click', (): void => { toggleMenu() })
+
+const fecharMenu = document.getElementById('icone-fechar-menu') as HTMLElement
+fecharMenu.addEventListener('click', (): void => { toggleMenu() })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
         "./src/**/*.{html,js,ts,jsx,tsx,vue}"
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'colorido': "url('/src/assets/img/bg-colorido.jpg')",
+            }
+        },
     },
     plugins: [],
 }
