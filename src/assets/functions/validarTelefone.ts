1351c934e7b9262da9d07d26fd02b1d5958e77ae
Douglas Moura <douglas12345.dsm@gmail.com>
2025-07-18 12:02:42 -0300

    refactor: Refinando o projeto e refatoranto código para melhor legibilidade
    - Link adicionados e testados
    - Ajustes de estilo e formatação
    - Correção de erros de digitação
    - Função de validação de telefone aprimorada

--- src/assets/functions/validarTelefone.ts
+++ src/assets/functions/validarTelefone.ts
@@ -1,4 +1,10 @@
 export const validarTelefone = (phoneNumber: string): boolean => {
-    const regex = /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][1-9]\)?\s?)?\d{4,5}[-\.\s]?\d{4})$/
-    return regex.test(phoneNumber)
+    if (!phoneNumber || typeof phoneNumber !== 'string') {
+        return false
+    } else if (phoneNumber.length === 0) {
+        return false
+    } else {
+        const regex = /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][1-9]\)?\s?)?\d{4,5}[-\.\s]?\d{4})$/
+        return regex.test(phoneNumber)
+    }
 }
